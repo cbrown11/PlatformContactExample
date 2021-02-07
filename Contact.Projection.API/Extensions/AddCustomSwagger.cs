9d7d10c243055e0ae9ba1069a2986267816eade1
Craig Brown <craigbrown77@hotmail.com>
2021-02-07 12:46:39 +0000

    Added Swagger

---
+++ Contact.Projection.API/Extensions/AddCustomSwagger.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace Contact.Projection.API.Extensions
+{
+    public static class MiddlewareExtensions
+    {
+        public static IServiceCollection AddCustomSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(cfg =>
+            {
+                cfg.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Contact Projection API",
+                    Version = "v3",
+                    Description = "Simple RESTful API built with ASP.NET Core 3.1 to show contacts read model",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Craig Brown",
+                        Url = new Uri("https://uk.linkedin.com/in/craig-brown-a6b3205")
+                    },
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                cfg.IncludeXmlComments(xmlPath);
+            });
+            return services;
+        }
+
+        public static IApplicationBuilder UseCustomSwagger(this IApplicationBuilder app)
+        {
+            app.UseSwagger().UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Contact Projection API");
+                options.DocumentTitle = "Contact Projection API";
+            });
+            return app;
+        }
+    }
+}
--- Contact.Projection.API/Startup.cs
+++ Contact.Projection.API/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using AutoMapper;
+using Contact.Projection.API.Extensions;
 
 namespace Contact.Projection.API
 {
@@ -22,6 +23,7 @@ namespace Contact.Projection.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCustomSwagger();
             services.AddSingleton<IContactService, ContactService>();
             services.AddSingleton<IContactRepository, ContactRepository>();
 
@@ -34,14 +36,16 @@ namespace Contact.Projection.API
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            
             app.UseHttpsRedirection();
+            app.UseCustomSwagger();
             app.UseRouting();
-           app.UseAuthorization();
+            app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
